5385597e5a92734ac37783dae89149bdeaa422d0
rrankawat <rakesh.rankawat@outlook.com>
2019-03-12 12:59:56 +0530

    2.1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,17 +9,31 @@ class Welcome extends CI_Controller {
 
 	public function ajax_upload() {
 
-		if(isset($_FILES['userfile']['name'])) {
-			$config['upload_path'] = './assets/upload/';
-			$config['allowed_types'] = 'jpg|jpeg|png|gif';
-			$this->load->library('upload', $config);
+		if(isset($_FILES['files']['name'])) {
 
-			if(!$this->upload->do_upload('userfile')) {
-				echo $this->upload->display_errors();
-			} else {
-				$data = array('upload_data' => $this->upload->data());
-				$post_image = $_FILES['userfile']['name'];
-				echo '<img src="'.base_url().'assets/upload/'.$post_image.'" />';
+			for($count=0; $count<count($_FILES['files']['name']); $count++) {
+				$_FILES['file']['name'] = $_FILES['files']['name'][$count];
+				$_FILES['file']['type'] = $_FILES['files']['type'][$count];
+				$_FILES['file']['tmp_name'] = $_FILES['files']['tmp_name'][$count];
+				$_FILES['file']['error'] = $_FILES['files']['error'][$count];
+				$_FILES['file']['size'] = $_FILES['files']['size'][$count];
+
+				$new_name = 'IMG'.time();
+				$config['file_name'] = $new_name;
+				$config['upload_path'] = './assets/upload/';
+				$config['allowed_types'] = 'jpg|jpeg|png|gif';
+				$this->load->library('upload', $config);
+				$this->upload->initialize($config);
+
+				if(!$this->upload->do_upload('file')) {
+					echo $this->upload->display_errors();
+				} else {
+					$data = array('upload_data' => $this->upload->data());
+					$post_image = $this->upload->data('file_name');
+					echo '<div class="col-md-3">';
+					echo '<img height="200" width="200" src="'.base_url().'assets/upload/'.$post_image.'" />';
+					echo '</div>';
+				}
 			}
 		}
 	}
@@ -28,5 +42,7 @@ class Welcome extends CI_Controller {
 		echo base_url();
 		echo "<br>";
 		echo site_url();
+		echo "<br>";
+		echo time();
 	}
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,20 +15,21 @@
 
     <body>
     	<div class="container">
-    		<?php 
-	    		$attributes = array(
-	    			'role' => 'form',
-	    			'class' => 'dropzone',
-	    			'id' => 'upload_form'
-	    		);
-	            echo form_open('welcome/ajax_upload', $attributes); 
-	            echo form_close();
-            ?>
+    		<br>
+    		<h2 align="center">Upload Multiple files using codeigniter</h2>
+
+    		<br>
+    		<div class="col-md-6" align="right">
+    			<label>Select Multiple files</label>
+    		</div>
+
+    		<div class="col-md-6">
+    			<input type="file" name="files" id="files" multiple />
+    		</div>
+    		<div style="clear: both"></div>
 
     		<br><br>
-    		<div id="uploaded_image">
-
-    		</div>
+    		<div id="uploaded_images" class="row"></div>
 		</div>
 
 	    <!-- Optional JavaScript -->
@@ -38,29 +39,47 @@
 	    <script>
 	        $(document).ready(function(){
 
-	            $("#upload_form").on('submit', function(e){
-	            	e.preventDefault();
-	            	if($('#image_file').val() == "") {
-	            		alert("Please select an image!");
-	            	} else {
+	            $("#files").change(function(){
+	            	var files = $("#files")[0].files;
+	            	var error = '';
+	            	var form_data = new FormData();
 
+	            	for(var count=0; count<files.length; count++){
+	            		var name = files[count].name;
+	            		var extension = name.split('.').pop().toLowerCase();
+	            		if(jQuery.inArray(extension, ['gif', 'png', 'jpg', 'jpeg']) == -1){
+	            			error += "Invalid " + count + " Image File";
+	            		} else {
+	            			form_data.append("files[]", files[count]);
+	            		}
+	            	}
+
+	            	if(error == '') {
 	            		$.ajax({
 	            			url: "<?php echo site_url('welcome/ajax_upload'); ?>",
 	            			method: "POST",
-	            			data: new FormData(this),
+	            			data: form_data,
 	            			contentType: false,
 	            			cache: false,
 	            			processData: false,
+	            			beforeSend: function() {
+	            				$('#uploaded_images').html('<label class="text-success">Uploading...</label>');
+	            			},
 	            			success: function(data) {
 	            				console.log('hi');
-	            				$('#uploaded_image').html(data);
+	            				$('#uploaded_images').html(data);
+	            				$('#files').val('');
 	            			},
-	            			error: function() {
+	            			error: function() {s
 	            				console.log('bye');
 	            			}
 	            		});
+	            	} else {
+	            		alert(error);
 	            	}
+
 	            });
+
 	        });
 	    </script>
   	</body>
